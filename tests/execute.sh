1c32e96a81ba3d3f296b7f8758ac69fd22853563
Raul Ferreira <raulmendesferreira@gmail.com>
2016-04-02 15:28:43 +0100

    Making sure i'm using bash.

--- tests/execute.sh
+++ tests/execute.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 for input in *.c
 do
-  output=`echo -en $input | sed s/\.c$//`
+  output=$(echo -en "$input" | sed s/\.c$//)
   echo -en "Running $input...\n"
   gcc -o $output $input -Wall -ansi -pedantic
   ../ANALYZE --exec $output > /dev/null
-#  ../../ANALYZE --exec $output
-  rm -f $output
+#  ../ANALYZE --exec $output
+  rm -f "$output"
   echo -en "\n"
 done
