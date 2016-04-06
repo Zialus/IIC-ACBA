69f1865f70355a40fd0d3d09014829a58b14284e
Raul Ferreira <raulmendesferreira@gmail.com>
2016-04-06 10:58:15 +0100

    Messing around with stuff

--- analyzer.c
+++ analyzer.c
@@ -1,52 +1,31 @@
+#include "Parser/cmdline.h"
 #include "SafeExec/safeexec.h"
 #include <stdio.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "Parser/cmdline.h"
+#include <string.h>
+#include <unistd.h>
 
-// #include <fstream>
-// #include <iostream>
-// #include <string>
+char* concat(char* s1, char* s2) {
+  char* result =
+      (char*)malloc(strlen(s1) + strlen(s2) + 1);  //+1 for the zero-terminator
+  // in real code you would check for errors in malloc here
+  strcpy(result, s1);
+  strcat(result, s2);
+  return result;
+}
 
-// int main(int argc, char **argv) {
-
-
-
- //    bool isCaseInsensitive = false;
- //    int opt;
- //    enum { CHARACTER_MODE, WORD_MODE, LINE_MODE } mode = CHARACTER_MODE;
-
-	// while ((opt = getopt(argc, argv, "ilw")) != -1) {
- //        switch (opt) {
- //        case 'i': isCaseInsensitive = true; break;
- //        case 'l': mode = LINE_MODE; break;
- //        case 'w': mode = WORD_MODE; break;
- //        default:
- //            fprintf(stderr, "Usage: %s [-ilw] [file...]\n", argv[0]);
- //            exit(EXIT_FAILURE);
- //        }
- //    }
+int main(int argc, char* argv[], char* envp[]) {
+  //-----------ARG PARSING STUF-----------START//
 
 
-//   int linecount = 0;
-//   std::string line;
-//   std::ifstream infile(argv[1]);
-//   if (infile) {
-//     while (getline(infile, line)) {
-//       std::cout << linecount << ": " << line
-//                 << std::endl; // supposing '\n' to be line end
-//       linecount++;
-//     }
-//   }
-//   else { printf("no file found\n");}
-//   infile.close();
-//   return 0;
-// }
+    for (int i = 0; i < argc; ++i)
+    {
+        printf("%s\n",argv[i]);
+    }
 
-int main(int argc, char *argv[],char *envp[]){
 
-  // cmdline parser burocracy
+
   struct gengetopt_args_info ai;
   if (cmdline_parser(argc, argv, &ai) != 0) {
     exit(1);
@@ -59,101 +38,87 @@ int main(int argc, char *argv[],char *envp[]){
   for (i = 0; i < ai.value_given; ++i) {
     printf("ai.value_arg[%d]: %d\n", i, ai.value_arg[i]);
   }
+  //-----------ARG PARSING STUF-----------END//
 
-  char *filename = ai.filename_arg;
+  //-----------FILE READING STUF----------START//
+
+  char* filename = ai.filename_arg;
   // open the file for reading
-  FILE *file = fopen(filename, "r");
-
+  FILE* file = fopen(filename, "r");
   // make sure the file opened properly
   if (NULL == file) {
     fprintf(stderr, "Cannot open file: %s\n", filename);
     return 1;
+  }
+
+  //-----------FILE READING STUF----------END//
+
+  printf("-------------------\n");
+
+  char* aaaa = (char*)"bla bla bla ";
+  char* bbbb = (char*)"not bla";
+
+  char* trololol = concat(aaaa, bbbb);
+
+  printf("%s\n", trololol);
+
+  printf("-------------------\n");
+
+  char x[1024];
+  /* assumes no word exceeds length of 1023 */
+  while (fscanf(file, " %1023s", x) == 1) {
+    printf("ESTOU NO WHILE\n");
+    puts(x);
+    int n = atoi(x);
+
+    printf("antes    %s\n", x);
+
+    printf("Number of args %d\n", n);
+
+    printf("depois    %s\n", x);
+
+    char x[1024]; /* assumes no word exceeds length of 1023 */
+    char y[1024];
+    while (fscanf(file, " %1023s", x) == 1 && fscanf(file, " %1023s", y)) {
+      puts(x);
+      puts(y);
+
+      char* _argv_[3];
+      int _argc_ = 2;
+      char* exec_string = (char*)"--exec";
+
+      // char* arg_for_safeexec_mofo = concat(exec_string, y);
+      // _argv_[1] = arg_for_safeexec_mofo;
+
+      char* arg_for_safeexec1 = exec_string;
+      char* arg_for_safeexec2 = y;
+
+      _argv_[1] = arg_for_safeexec1;
+      _argv_[2] = arg_for_safeexec2;
+
+      printf("\nSTART--------\n");
+
+    for (int i = 1; i <= _argc_; ++i)
+    {
+        printf("arg %d: %s\n",i,_argv_[i]);
     }
 
+    printf("---manda para o safeexec---\n\n");
 
-    char x[1024];
-    /* assumes no word exceeds length of 1023 */
-    while (fscanf(file, " %1023s", x) == 1) {
-        puts(x);
+      RESULTS res;
+      res = safeexec(_argc_, _argv_, envp);
+
+      if (res != NULL) {
+        printf("-----\n\n");
+        printf("%d\n", res->mem);
+        printf("%d\n", res->timer);
+
+        printf("END--------\n");
+      } else {
+        printf("OOOOPS--------\n");
+        return 1;
+      }
     }
-
-
-	   	RESULTS res;
-
-		char* arg_for_safeexec1 = "--exec";
-		char* arg_for_safeexec2 = x;
-
-		char* _argv_[2];
-
-		_argv_[0] = arg_for_safeexec1;
-		_argv_[1] = arg_for_safeexec2;
-
-		int _argc_=2;
-
-		
-		res = safeexec(_argc_,_argv_, envp);
-		
-		if(res!=NULL){
-			printf("-----\n\n");
-			printf("%d\n",res->mem);
-			printf("%d\n",res->timer);
-			return 0;
-		}
-
-		return 1;
-
-
-
-	// // set up the buffer to read the line into. Don't worry too much
- //    // if some of the lines are longer than 80 characters - the buffer
- //    // will be made bigger if need be.
- //    size_t buffer_size = 80;
- //    char *buffer = (char*) malloc(buffer_size * sizeof(char));
-
- //    // read each line and print it to the screen
- //    int line_number = 0;
- //    while(-1 != getline(&buffer, &buffer_size, file))
- // 	{
-	//     printf("%d: %s", ++line_number, buffer);
- //    }
- //    fflush(stdout);
-
- //    // make sure we close the file and clean up the buffer when we're
- //    // finished
- //    fclose(file);
- //    free(buffer);
-
- //    return 0;
-
-
-
-
-
-
-
-
-
-
-
-
-
-	// char buf[256];
-	// while (fgets (buf, sizeof(buf), stdin)) {
-	// 	printf("line: %s", buf);
-	// }
-	// if (ferror(stdin)) {
-	// 	fprintf(stderr,"Oops, error reading stdin\n");
-	// 	abort();
-	// }
-	// return 0;
-
-
-
-
-
-
-
-
-
-
+  }
+  return 0;
 }
