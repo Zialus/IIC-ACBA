f98a74372665531fd4182c01c3b92a286e054194
Raul Ferreira <raulmendesferreira@gmail.com>
2017-10-28 22:02:12 +0100

    Better code organization.

--- pearson.cpp
+++ pearson.cpp
@@ -3,18 +3,16 @@
 #include <stdlib.h>
 
 double pearson(double x[], double y[], int n) {
-  int i;
-
-  double r, xx[n], yy[n], nr = 0, dr_1 = 0, dr_2 = 0, dr_3 = 0, dr = 0;
+  double xx[n], yy[n];
 
   double sum_y = 0, sum_yy = 0, sum_xy = 0, sum_x = 0, sum_xx = 0;
 
-  for (i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++) {
     xx[i] = x[i] * x[i];
     yy[i] = y[i] * y[i];
   }
 
-  for (i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++) {
     sum_x += x[i];
     sum_y += y[i];
     sum_xx += xx[i];
@@ -22,14 +20,14 @@ double pearson(double x[], double y[], int n) {
     sum_xy += x[i] * y[i];
   }
 
-  nr = (n * sum_xy) - (sum_x * sum_y);
+  double nr = (n * sum_xy) - (sum_x * sum_y);
   double sum_x2 = sum_x * sum_x;
   double sum_y2 = sum_y * sum_y;
-  dr_1 = (n * sum_xx) - sum_x2;
-  dr_2 = (n * sum_yy) - sum_y2;
-  dr_3 = dr_1 * dr_2;
-  dr = sqrt(dr_3);
-  r = (nr / dr);
+  double dr_1 = (n * sum_xx) - sum_x2;
+  double dr_2 = (n * sum_yy) - sum_y2;
+  double dr_3 = dr_1 * dr_2;
+  double dr = sqrt(dr_3);
+  double r = (nr / dr);
 
   return r;
 }
